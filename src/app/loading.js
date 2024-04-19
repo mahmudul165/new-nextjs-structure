22427a111d5ed62e4a6921a3a0f962db4efb8ecf
Mahmudul Hasan <61279339+mahmudul165@users.noreply.github.com>
2024-04-19 22:53:37 +0600

    product name slice perfectly

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,4 +1,9 @@
+
+import Skeleton from 'react-loading-skeleton'
 export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
-    return <LoadingSkeleton />
+    return <Skeleton height={50} width={300} />
   }
+ 
+
+ 
