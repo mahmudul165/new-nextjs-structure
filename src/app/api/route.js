f72e1201d281ef1f03c3308b9f605fa778e7a9bf
Mahmudul Hasan <61279339+mahmudul165@users.noreply.github.com>
2024-04-19 23:00:32 +0600

    product name slice perfectly

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,3 +1,3 @@
-module.exports = async function handler(req, res) {
-    // your code here
+export default async function handler(req, res) {
+    res.status(200).json({ message: 'Hello, World!' })
   }
